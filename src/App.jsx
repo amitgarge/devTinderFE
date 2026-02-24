1a780fa540b041a7a440c7b60540da4f88698f58
Amit <amit.garge19@gmail.com>
2026-02-24 16:56:36 +0530

    Fixed BrowserRouter Import

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Body from "./components/Body";
 import Profile from "./components/Profile";
 import Login from "./components/Login";
